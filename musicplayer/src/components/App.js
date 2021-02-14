47d2fba3c330cdbfc68c4e9da2bfc86148ff3a18
SalarC123 <Tabinda@chaudrys-iMac.local>
2021-02-14 15:24:10 -0600

    react router and mobile responsiveness

--- musicplayer/src/components/App.js
+++ musicplayer/src/components/App.js
@@ -3,6 +3,10 @@ import React, { useState } from 'react'
 import DisplaySong from './DisplaySong'
 import { SongProvider } from './SongContext'
 import SongModal from './SongModal'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Contact from './Contact';
+import Instructions from './Instructions'
+import hamburger from '../Images/hamburger.png'
 
 function App() {
 
@@ -23,23 +27,62 @@ function App() {
     setModalVisibility(display)
   }
 
+  const dropdownStartPosition = '-600px'
+  const [hamburgerVisibility, setHamburgerVisibility] = useState({flexDirection:'column', position:0, left:0, margin:0, top: dropdownStartPosition})
+  const handleHamburgerVisibility = (newState) => {
+    if (hamburgerVisibility.top == dropdownStartPosition) {
+      setHamburgerVisibility({...hamburgerVisibility, top:'0'})
+    } else {
+      setHamburgerVisibility({...hamburgerVisibility, top: dropdownStartPosition})
+    }
+  }
+
   return (
-    <SongProvider>
-      <SongModal 
-          modalVisibility={modalVisibility} 
-          handleModalVisibility={handleModalVisibility} 
-          allSongs={allSongs}
-          handleAllSongs={handleAllSongs}
-      />
-      <div className="open-library-wrapper">
-        <h2 onClick={moveSidebar} className="open-library">Toggle Library</h2>
-      </div>
-      <DisplaySong/>
-      <div className={`sidebar ${sidebarPosition?'sidebar-move':''}`}>
-        <button onClick={() => handleModalVisibility('flex')} className="add-new-song">Add Song</button>
-        {allSongs.map((song) => (song))}
-      </div>
-    </SongProvider>
+    <Router>
+      <SongProvider>
+        <SongModal 
+            modalVisibility={modalVisibility} 
+            handleModalVisibility={handleModalVisibility} 
+            allSongs={allSongs}
+            handleAllSongs={handleAllSongs}
+        />
+        {/* <h1 className='project-title'>Interactive Music Player</h1> */}
+        <img onClick={() => handleHamburgerVisibility()} className='hamburger' src={hamburger} alt="Hamburger Menu"/>
+        <div className="open-library-wrapper" style={hamburgerVisibility}>
+          <Link style={{textDecoration:'none'}} to='/'>
+            <h2>Home</h2>
+          </Link>
+          <Link style={{textDecoration:'none'}} to='/instructions'>
+            <h2>Instructions</h2>
+          </Link>
+          <Link style={{textDecoration:'none'}} to='/contact'>
+            <h2>Contact</h2>
+          </Link>
+
+          <h2 onClick={moveSidebar}>Toggle Library</h2>
+        </div>
+
+        <Switch>
+
+          <Route exact path='/'>
+            <DisplaySong/>
+            <div className={`sidebar ${sidebarPosition?'sidebar-move':''}`}>
+              <button onClick={() => handleModalVisibility('flex')} className="add-new-song">Add Song</button>
+              {allSongs.map((song) => (song))}
+            </div>
+          </Route>
+
+          <Route exact path='/instructions'>
+            <Instructions/>
+          </Route>
+
+          <Route exact path='/contact'>
+            <Contact/>
+          </Route>
+
+        </Switch>
+      </SongProvider>
+    </Router>
   );
 }
 
--- musicplayer/src/components/LibrarySong.js
+++ musicplayer/src/components/LibrarySong.js
@@ -1,27 +1,35 @@
 import './css/LibrarySong.css'
 import { useContext } from 'react'
 import { SongContext } from './SongContext'
+import { unmountComponentAtNode } from 'react-dom';
 
-function LibrarySong(props) {
+function LibrarySong({ name, artist, image, favorite, audio, key }) {
     const [songInfo, setSongInfo] = useContext(SongContext)
+    // allSongs.filter((elem) => console.log(elem, key))       //elem.key != key
 
     const switchSong = () => {
         setSongInfo({
-            name: props.name, 
-            artist: props.artist, 
-            image: props.image, 
-            favorite: props.favorite,
-            audio: props.audio
+            name: name, 
+            artist: artist, 
+            image: image, 
+            favorite: favorite,
+            audio: audio
         })
+    }
+
+    // Removes library songs
+    function removeNode(e, node) {
+        unmountComponentAtNode(node)
+        e.target.parentElement.parentElement.removeChild(node)
     }
     
     return (
         <div className='library-song'>
-            <button className='delete-from-library'>X</button>
-            <h1>{props.name}</h1>
-            <p>{props.artist}</p>
-            <div className='library-heart' style={{backgroundColor:props.favorite?'red':'white'}}></div>
-            <img className='switch-image' src={props.image} alt=""/>
+            <button onClick={(e) => removeNode(e, e.target.parentElement)} className='delete-from-library'></button>
+            <h1>{name}</h1>
+            <p>{artist}</p>
+            <div className='library-heart' style={{backgroundColor:favorite?'red':'white'}}></div>
+            <img className='switch-image' src={image} alt=""/>
             <button className='switch-button' onClick={switchSong}>CHOOSE</button>
         </div>
     )
