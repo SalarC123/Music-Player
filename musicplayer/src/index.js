f7e32b2572541986b0b37f33d90a9118abc85575
SalarC123 <Tabinda@chaudrys-iMac.local>
2021-03-06 18:26:52 -0600

    wrapper for modal visibility reducer context

--- musicplayer/src/index.js
+++ musicplayer/src/index.js
@@ -4,14 +4,17 @@ import './components/css/index.css';
 import App from './components/App';
 import { AllSongsProvider } from './components/AllSongsContext';
 import {PlaylistCollectionProvider} from './components/PlaylistCollectionContext'
+import { VisibilityProvider } from './components/VisibilityContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <PlaylistCollectionProvider>
-      <AllSongsProvider>
-        <App />
-      </AllSongsProvider>
-    </PlaylistCollectionProvider>
+    <VisibilityProvider>
+      <PlaylistCollectionProvider>
+        <AllSongsProvider>
+          <App />
+        </AllSongsProvider>
+      </PlaylistCollectionProvider>
+    </VisibilityProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
